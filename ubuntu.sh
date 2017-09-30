d2e576e136a9cde3af15c710ab75d554fb809d57
Dave Coleman <davetcoleman@gmail.com>
2017-09-30 15:48:41 -0600

    Update text

---
+++ ubuntu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# bootstrap setting up the bitchin_unix environment for ubuntu
+
+cat <<EOF
+
+---------------------------------------------
+Welcome to the BITCHIN UNIX Bootstrap script!
+---------------------------------------------
+An internal tool of PICKNIK Robotics.
+
+TODO: automatically determine if there have been any changes to bitchin_unix repo, if it even exists
+
+EOF
+
+# Ensure there are no changes to current bitchin_unix repo
+read -p "This script will overwrite any changes in bitchin_unix, are you sure? Press Ctrl-C to cancel" $dummy
+
+if [[ $EUID -ne 0 ]]; then
+    echo "You are not a root user"
+    sudo apt-get install -y git
+else # assume we are docker
+    apt-get update
+    apt-get install -y git sudo
+fi
+
+unalias git >/dev/null 2>/dev/null # redirects error msg output
+cd ~
+
+rm -rf bitchin_unix
+git clone https://github.com/PickNikRobotics/bitchin_unix.git
+. bitchin_unix/install/ubuntu.sh
